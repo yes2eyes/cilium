2b861c8370bb8c6e6631cbe651d13c8b3bf31001
Jarno Rajahalme <jarno@covalent.io>
2020-09-11 17:32:55 +0300

    test: Lauch runtime VM in the same network as k8s for the same job
    Configure cilium-k8s#{$BUILD_NUMBER}-#{$JOB_NAME}-#{$K8S_VERSION}
    private network also for the runtime VM so that it has connectivity to
    k8s nodes when run at the same time. This is needed for VM support
    testing.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

---
+++ test/vagrant-local-start-runtime.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export K8S_VERSION=${K8S_VERSION:-1.19}
+export LOCAL_BOX=k8s-box
+
+echo "starting runtime vm"
+export SERVER_BOX=$LOCAL_BOX
+export SERVER_VERSION=0
+unset PRELOAD_VM
+vagrant up runtime --provision
